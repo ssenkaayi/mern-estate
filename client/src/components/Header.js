73b413b989a9a19a5397444a1d3f51ed1e5311a7
ssenkaayi <ssenkaaayi@gmail.com>
2023-11-26 12:43:01 +0300

    compelete profile image upload to firebase

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,10 +10,10 @@ export default function Header() {
 
     <header className='big-slate-200 shadow-md'>
 
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='flex justify-between  flex-shrink-0 items-center max-w-6xl mx-auto p-3'>
 
             <Link to='/'>
-                <h1 className='font-bold text-sm:text-xt flex flex-wrap'>
+                <h1 className='font-bold text-sm:text-xt flex flex-wrap flex-shrink-0'>
                     <span className='text-slate-500'>Sahand</span>
                     <span className='text-slate-700'>Estate</span>
                 </h1>
@@ -25,13 +25,13 @@ export default function Header() {
 
                 <input type='text' 
                 placeholder='search' 
-                className='bg-transparent focus:outline-none w-20 sm:w-80'></input>
+                className='bg-transparent focus:outline-none w-20  sm:w-80'></input>
 
                 <FaSearch className='text-slate-500' ></FaSearch>
                 
             </form>
 
-            <ul className='flex gap-4 text-slate-700'>
+            <ul className='flex flex-shrink-0 gap-4 text-slate-700'>
 
                 <Link to='/'>
                     <li className='hidden sm:inline hover:underline'>Home</li>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,92 @@
 import React from 'react'
 import {useSelector } from 'react-redux/es/hooks/useSelector'
+import { useRef,useState,useEffect } from 'react';
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../firebase';
+
 
 export default function Profile() {
 
   const{currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const[file,setFile] = useState(undefined);
+  const[filePerc,setFilePerc] = useState(0)
+  const[uploadError,setUploadError] = useState(false);
+  const[formData,setFormData]= useState({})
+
+  console.log(formData)
+  // console.log(filePerc)
+  // console.log(uploadError)
+
+
+  useEffect(()=>{
+
+    if(file){
+      handleFileUpload(file);
+    }
+
+  },[file]);
+
+  const handleFileUpload = (file)=>{
+
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage,fileName); 
+    const uploadTask = uploadBytesResumable(storageRef,file);
+
+    uploadTask.on('state_changed',
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred/
+      snapshot.totalBytes)*100;
+      setFilePerc(Math.round(progress))
+      
+    },
+    (error) =>{
+      setUploadError(true);
+    },
+    ()=>{getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadURL)=>
+        setFormData({...formData,avatar:downloadURL})
+      );}
+    );
+
+  
+  }
+
   return (
 
     <div className='p-3 max-w-lg mx-auto'>
+
         <h1 className='
         text-3xl font-semibold
         text-center my-7'
         > Profile </h1>
 
+
+
         <form className='flex flex-col gap-4'>
-          <img className='rounded-full h-24 w-24 
+
+          <input  onChange={(e)=>setFile(e.target.files[0])}
+          ref={fileRef} 
+          type='file' accept='image/*' hidden/>
+
+          <img
+          onClick={()=>{fileRef.current.click()}} 
+          className='rounded-full h-24 w-24 
           object-cover cursor-pointer
-          self-center mt-2' src={currentUser.avatar} alt='profile'/>
+          self-center mt-2' 
+          src={formData.avatar || currentUser.avatar} alt='profile'/>
+          <p className='text-center'>
+            {uploadError?(
+              <span className='text-red-7oo'>Error uploading image</span>
+            ):
+            filePerc > 0 && filePerc < 100 ?(<span className='text-slate-700'>{'uploading ${filePerc}%'}</span>):
+            filePerc===100?(<span className='text-green-700'>successfully uploaded!</span>):('')}
+
+          </p>
+         
           <input type='text' placeholder='username' id='username' className='
           border p-3 rounded-lg'/>
-        
 
           <input type='text' placeholder='email' id='email' className='
           border p-3 rounded-lg'/>
